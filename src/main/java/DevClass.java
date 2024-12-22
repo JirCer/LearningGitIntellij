8960b52b30c55cbd16f9426ca6e2eb60157da793
JirCer <jiricerny7@gmail.com>
2024-12-22 12:28:10 +0100

    Update DevClass.java
    pridal som do branche feature

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,7 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass feature");
+        //toto pridavam k feature
     }
 
 }
