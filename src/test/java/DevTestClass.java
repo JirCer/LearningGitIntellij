c207be46850e200e183e16339c8c2c9e6e2633a0
JirCer <jiricerny7@gmail.com>
2024-12-22 11:48:12 +0100

    Update DevTestClass.java

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,6 +1,6 @@
 public class DevTestClass {
     public static void main(String[] args) {
-        System.out.println("DevClassTest + stream");
+        System.out.println("DevClassTest - stream");
         //pridavam nieco
         //pridal som branchu a stream 
     }
