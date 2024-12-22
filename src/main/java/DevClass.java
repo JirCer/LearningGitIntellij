ee0dd4ec66ce90e3fadf8f694f213d61578dccfa
JirCer <jiricerny7@gmail.com>
2024-12-22 13:12:45 +0100

    Update DevClass.java
    pridane feature2

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,8 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass feature");
+        System.out.println("DevClass feature 2");
         //toto pridavam k feature
+        //feature 2
     }
 
 }
